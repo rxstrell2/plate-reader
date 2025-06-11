769d1796966561d3c6c5429c6699895b7c2fe92b
Tim <mazemaze2585@gmail.com>
2025-06-10 19:53:50 -0700

    Обновлён app.py для поддержки Render

--- app.py
+++ app.py
@@ -34,4 +34,6 @@ def upload():
 
 # This block is only used for local testing. Render uses gunicorn.
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
